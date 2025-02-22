014832766615fd430c7bab8e9fdf5f23456d01d8
Alex Sobiek <alex@alexsobiek.com>
2025-02-22 15:37:25 -0600

    Added additional arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,19 @@
 # Define the command and add arguments if environment variables are set
 CMD="python /opt/sip2mqtt/sip2mqtt.py"
 
-[ -n "$MQTT_ADDRESS" ] && CMD="$CMD -a$MQTT_ADDRESS"
-[ -n "$MQTT_PORT" ] && CMD="$CMD -t$MQTT_PORT"
-[ -n "$MQTT_USERNAME" ] && CMD="$CMD -u$MQTT_USERNAME"
-[ -n "$MQTT_PASSWORD" ] && CMD="$CMD -p$MQTT_PASSWORD"
-[ -n "$SIP_DOMAIN" ] && CMD="$CMD -d$SIP_DOMAIN"
-[ -n "$SIP_USERNAME" ] && CMD="$CMD -n$SIP_USERNAME"
-[ -n "$SIP_PASSWORD" ] && CMD="$CMD -s$SIP_PASSWORD"
-
+[ -n "$MQTT_ADDRESS" ] && CMD="$CMD --mqtt_domain $MQTT_ADDRESS"
+[ -n "$MQTT_PORT" ] && CMD="$CMD --mqtt_port $MQTT_PORT"
+[ -n "$MQTT_KEEPALIVE" ] && CMD="$CMD --mqtt_keepalive $MQTT_KEEPALIVE"
+[ -n "$MQTT_PROTOCOL" ] && CMD="$CMD --mqtt_protocol $MQTT_PROTOCOL"
+[ -n "$MQTT_USERNAME" ] && CMD="$CMD --mqtt_username $MQTT_USERNAME"
+[ -n "$MQTT_PASSWORD" ] && CMD="$CMD -mqtt_password $MQTT_PASSWORD"
+[ -n "$MQTT_TOPIC" ] && CMD="$CMD --mqtt_topic $MQTT_TOPIC"
+[ -n "$SIP_DOMAIN" ] && CMD="$CMD --sip_domain $SIP_DOMAIN"
+[ -n "$SIP_USERNAME" ] && CMD="$CMD --sip_username $SIP_USERNAME"
+[ -n "$SIP_PASSWORD" ] && CMD="$CMD --sip_password $SIP_PASSWORD"
+[ -n "$SIP_DISPLAY" ] && CMD="$CMD --sip_display $SIP_DISPLAY"
+[ -n "$LOG_LEVEL" ] && CMD="$CMD --log_level $LOG_LEVEL"
+[ -n "$VERBOSITY" ] && CMD="$CMD --verbosity $VERBOSITY"
+ 
 # Execute the command
 exec $CMD
